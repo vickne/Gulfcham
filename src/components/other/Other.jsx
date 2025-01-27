3765da1db15c8be48effede4648619874817131b
vickne <30363517+vickne@users.noreply.github.com>
2025-01-27 19:16:01 +0300

    Update Other.jsx

--- src/components/other/Other.jsx
+++ src/components/other/Other.jsx
@@ -1,159 +1,74 @@
-import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import {useRef} from "react";
 
-const Other = ({ i,j,k expanded, setExpanded, title, description }) => {
-  const isOpen = i === expanded;
-  const isOpen = j === expanded;
-  const isOpen = k === expanded;
+const items = [
+    {
+        id:1,
+        title:"Ethics",
+        img:"/ethics.png",
+        desc:"Uncompromising Integrity, Honesty & Fairness",
+    },
+    {
+        id:2,
+        title:"Sustainability",
+        img:"/sustainability.png",
+        desc:"We plan and act for the future & continuous development of our company and community.",
+    },
+    {
+        id:3,
+        title:"Mutual Respect",
+        img:"/mutual-respect.png",
+        desc:"We do the right thing and treat each other with respect.Commitment to international standards of safety and quality. Commitment to Time and Cost.",
+    },
+    {
+        id:4,
+        title:"Professionalism",
+        img:"/professionalism.png",
+        desc:"We maintain the highest standards of Excellency, corporate behaviour and performance",
+    },
+];
+
+const Single = ({item}) =>{
+    const ref = useRef();
+
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        //offset: ["start start", "end start"]
+    });
+
+    const y = useTransform(scrollYProgress, [0,1],[-300, 300]);
+    
+    return (
+    <section>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                <motion.div className="textContainer" style={{y}}>
+                    <h2>{item.title}</h2>
+                    <p>{item.desc}</p>
+                </motion.div>
+                </div>
+            </div>
+        </section>
+        );
+}
+const Other = () => {
+
+  const ref = useRef();
   return (
-    <>
-      <motion.div
-        initial={false}
-        animate={{
-          backgroundColor: isOpen ? "#fcd125" : "#003f66",
-        }}
-        onClick={() => setExpanded(isOpen ? false : i)}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "2rem 4rem",
-          cursor: "pointer",
-          borderTop:" 2px solid",
-        }}
-      >
-        {<h1>HOT-MELT ADHESIVE (HMA)</h1>}
-      </motion.div>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.section
-            // Add animations for the accordion content
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
-            style={{
-              backgroundColor: "#080808c9",
-              padding: "0 2rem",
-            }}
-          >
-            {
-                <ul>
-                    <li>Technomelt Prime 200</li>
-                    <li>Technomelt Prime 400</li>
-                    <li>Technomelt Supra 100 Plus 22</li>
-                    <li>Technomelt 3101</li>
-                    <li>Technomelt 300</li>
-                    <li>Technomelt 2401</li>
-                    <li>Technomelt 3625</li>
-                    <li>Technomelt 2414</li>
-                    <li>Technomelt Cleaner 103</li>
-                </ul>
-            }
-          </motion.section>
-        )}
-      </AnimatePresence>
-      <motion.div
-        initial={false}
-        animate={{
-          backgroundColor: isOpen ? "#fcd125" : "#003f66",
-        }}
-        onClick={() => setExpanded(isOpen ? false : j)}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "2rem 4rem",
-          cursor: "pointer",
-          borderTop:" 2px solid",
-        }}
-      >
-        {<h1>AQUENCE-WATER BASED ADHESIVE</h1>}
-      </motion.div>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.section
-            // Add animations for the accordion content
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
-            style={{
-              backgroundColor: "#080808c9",
-              padding: "0 2rem",
-            }}
-          >
-            {
-                <ul>
-                    <li>Aquence LAM 7060</li>
-                    <li>Aquence 7093</li>
-                    <li>Aquence BG 6291</li>
-                    <li>Aquence 48345</li>
-                    <li>Aquence GA 060</li>
-                    <li>Aquence LA 10</li>
-                    <li>Aquence 709 H</li>
-                    <li>Aquence Seal 0106</li>
-                </ul>
-            }
-          </motion.section>
-        )}
-      </AnimatePresence>
-      <motion.div
-        initial={false}
-        animate={{
-          backgroundColor: isOpen ? "#fcd125" : "#003f66",
-        }}
-        onClick={() => setExpanded(isOpen ? false : k)}
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "2rem 4rem",
-          cursor: "pointer",
-          borderTop:" 2px solid",
-        }}
-      >
-        {<h1>OTHER CHEMICALS</h1>}
-      </motion.div>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.section
-            // Add animations for the accordion content
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
-            style={{
-              backgroundColor: "#080808c9",
-              padding: "0 2rem",
-            }}
-          >
-            {
-                <ul>
-                    <li>NATURAL HYDRAULIC LIME</li>
-                    <li>FABRIC SOFTENER BASE</li>
-                    <li>SOFTENER SALT</li>
-                    <li>CAUSTIC SODA</li>
-                    <li>THINNER</li>
-                </ul>
-            }
-          </motion.section>
-        )}
-      </AnimatePresence>
-    </>
-  );
-};
+    <div className="portfolio" ref={ref}>
+        <div className="progress">
+            <h2>Chemical</h2>
+            <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        </div>
+        {items.map(item=>(
+            <Single item ={ item} key= {item.id} />
+        ))}
+    </div>
+  )
+}
 
 export default Other

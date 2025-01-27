4200e700006ce1b9fac8c40cc0ec63487655e444
vickne <30363517+vickne@users.noreply.github.com>
2025-01-27 20:55:05 +0300

    Update Other.jsx

--- src/components/other/Other.jsx
+++ src/components/other/Other.jsx
@@ -2,73 +2,41 @@ import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import {useRef} from "react";
 
-const items = [
-    {
-        id:1,
-        title:"Ethics",
-        img:"/ethics.png",
-        desc:"Uncompromising Integrity, Honesty & Fairness",
-    },
-    {
-        id:2,
-        title:"Sustainability",
-        img:"/sustainability.png",
-        desc:"We plan and act for the future & continuous development of our company and community.",
-    },
-    {
-        id:3,
-        title:"Mutual Respect",
-        img:"/mutual-respect.png",
-        desc:"We do the right thing and treat each other with respect.Commitment to international standards of safety and quality. Commitment to Time and Cost.",
-    },
-    {
-        id:4,
-        title:"Professionalism",
-        img:"/professionalism.png",
-        desc:"We maintain the highest standards of Excellency, corporate behaviour and performance",
-    },
-];
 
-const Single = ({item}) =>{
-    const ref = useRef();
-
-    const {scrollYProgress} = useScroll({
-        target: ref,
-        //offset: ["start start", "end start"]
-    });
-
-    const y = useTransform(scrollYProgress, [0,1],[-300, 300]);
-    
-    return (
-    <section>
-            <div className="container">
-                <div className="wrapper">
-                    <div className="imageContainer" ref={ref}>
-                        <img src={item.img} alt="" />
-                    </div>
-                <motion.div className="textContainer" style={{y}}>
-                    <h2>{item.title}</h2>
-                    <p>{item.desc}</p>
-                </motion.div>
-                </div>
-            </div>
-        </section>
-        );
-}
 const Other = () => {
+ const chemlist = [
+    { name: 'HOT-MELT ADHESIVE (HMA)', lists: ['Aquence LAM 7060', 'Aquence 7093',
+'Aquence BG 6291',
+'Aquence 48345',
+'Aquence GA 060',
+'Aquence LA 10',
+'Aquence 709 H',
+'Aquence Seal 0106'] },
+    { name: 'AQUENCE-WATER BASED ADHESIVE', lists: ['Aquence LAM 7060', 'Aquence 7093',
+'Aquence BG 6291',
+'Aquence 48345',
+'Aquence GA 060',
+'Aquence LA 10',
+'Aquence 709 H',
+'Aquence Seal 0106'] },
+    { name: 'Other Chemicals', lists: ['NATURAL HYDRAULIC LIME', 'FABRIC SOFTENER BASE', 'SOFTENER SALT', 'CAUSTIC SODA','THINNER'] },
+  ];
 
-  const ref = useRef();
   return (
-    <div className="portfolio" ref={ref}>
-        <div className="progress">
-            <h2>Chemical</h2>
-            <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+    <div>
+      <h1>Other</h1>
+      {chemlists.map((chemlist, index) => (
+        <div key={index}>
+          <h2>{chemlist.name}</h2>
+          <ul>
+            {chemlist.lists.map((list, index) => (
+              <li key={index}>{list}</li>
+            ))}
+          </ul>
         </div>
-        {items.map(item=>(
-            <Single item ={ item} key= {item.id} />
-        ))}
+      ))}
     </div>
-  )
+  );
 }
 
 export default Other

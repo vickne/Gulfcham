c72026fadf3100bc989f5e04bd507632ba3a73b8
vickne <30363517+vickne@users.noreply.github.com>
2025-01-27 21:10:27 +0300

    Update Other.jsx

--- src/components/other/Other.jsx
+++ src/components/other/Other.jsx
@@ -1,41 +1,156 @@
 import React from 'react';
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, AnimatePresence } from 'framer-motion';
 
-
-const Other = () => {
- const chemlist = [
-    { name: 'HOT-MELT ADHESIVE (HMA)', lists: ['Aquence LAM 7060', 'Aquence 7093',
-'Aquence BG 6291',
-'Aquence 48345',
-'Aquence GA 060',
-'Aquence LA 10',
-'Aquence 709 H',
-'Aquence Seal 0106'] },
-    { name: 'AQUENCE-WATER BASED ADHESIVE', lists: ['Aquence LAM 7060', 'Aquence 7093',
-'Aquence BG 6291',
-'Aquence 48345',
-'Aquence GA 060',
-'Aquence LA 10',
-'Aquence 709 H',
-'Aquence Seal 0106'] },
-    { name: 'Other Chemicals', lists: ['NATURAL HYDRAULIC LIME', 'FABRIC SOFTENER BASE', 'SOFTENER SALT', 'CAUSTIC SODA','THINNER'] },
-  ];
-
+const Other = ({ i,j,k expanded, setExpanded, title, description }) => {
   return (
-    <div>
-      <h1>Other</h1>
-      {chemlists.map((chemlist, index) => (
-        <div key={index}>
-          <h2>{chemlist.name}</h2>
-          <ul>
-            {chemlist.lists.map((list, index) => (
-              <li key={index}>{list}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div
+        initial={false}
+        animate={{
+          backgroundColor: isOpen ? "#fcd125" : "#003f66",
+        }}
+        onClick={() => setExpanded(isOpen ? false : i)}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "2rem 4rem",
+          cursor: "pointer",
+          borderTop:" 2px solid",
+        }}
+      >
+        {<h1>HOT-MELT ADHESIVE (HMA)</h1>}
+      </motion.div>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            // Add animations for the accordion content
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
+            style={{
+              backgroundColor: "#080808c9",
+              padding: "0 2rem",
+            }}
+          >
+            {
+                <ul>
+                    <li>Technomelt Prime 200</li>
+                    <li>Technomelt Prime 400</li>
+                    <li>Technomelt Supra 100 Plus 22</li>
+                    <li>Technomelt 3101</li>
+                    <li>Technomelt 300</li>
+                    <li>Technomelt 2401</li>
+                    <li>Technomelt 3625</li>
+                    <li>Technomelt 2414</li>
+                    <li>Technomelt Cleaner 103</li>
+                </ul>
+            }
+          </motion.section>
+        )}
+      </AnimatePresence>
+      <motion.div
+        initial={false}
+        animate={{
+          backgroundColor: isOpen ? "#fcd125" : "#003f66",
+        }}
+        onClick={() => setExpanded(isOpen ? false : j)}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "2rem 4rem",
+          cursor: "pointer",
+          borderTop:" 2px solid",
+        }}
+      >
+        {<h1>AQUENCE-WATER BASED ADHESIVE</h1>}
+      </motion.div>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            // Add animations for the accordion content
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
+            style={{
+              backgroundColor: "#080808c9",
+              padding: "0 2rem",
+            }}
+          >
+            {
+                <ul>
+                    <li>Aquence LAM 7060</li>
+                    <li>Aquence 7093</li>
+                    <li>Aquence BG 6291</li>
+                    <li>Aquence 48345</li>
+                    <li>Aquence GA 060</li>
+                    <li>Aquence LA 10</li>
+                    <li>Aquence 709 H</li>
+                    <li>Aquence Seal 0106</li>
+                </ul>
+            }
+          </motion.section>
+        )}
+      </AnimatePresence>
+      <motion.div
+        initial={false}
+        animate={{
+          backgroundColor: isOpen ? "#fcd125" : "#003f66",
+        }}
+        onClick={() => setExpanded(isOpen ? false : k)}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "2rem 4rem",
+          cursor: "pointer",
+          borderTop:" 2px solid",
+        }}
+      >
+        {<h1>OTHER CHEMICALS</h1>}
+      </motion.div>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.section
+            // Add animations for the accordion content
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
+            style={{
+              backgroundColor: "#080808c9",
+              padding: "0 2rem",
+            }}
+          >
+            {
+                <ul>
+                    <li>NATURAL HYDRAULIC LIME</li>
+                    <li>FABRIC SOFTENER BASE</li>
+                    <li>SOFTENER SALT</li>
+                    <li>CAUSTIC SODA</li>
+                    <li>THINNER</li>
+                </ul>
+            }
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </>
   );
-}
+};
 
 export default Other
